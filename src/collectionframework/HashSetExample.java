b1c7d0a2d6e899644d110df700cbe556e49732f5
Jayden <jinkyukim.dev@gmail.com>
2020-06-08 14:39:38 +0900

    Set Collection

---
+++ src/collectionframework/HashSetExample.java
@@ -0,0 +1,43 @@
+package collectionframework;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HashSetExample {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<String>();
+
+        set.add("Java");
+        set.add("JDBC");
+        set.add("Servelet/JSP");
+        set.add("Java");
+        set.add("iBATIS");
+
+        System.out.println(set);
+
+        int size = set.size();
+        System.out.println("총 객체수: " + size);
+
+        Iterator<String> iterator = set.iterator();
+        while(iterator.hasNext()) {
+            String element = iterator.next();
+            System.out.println("\t" + element);
+        }
+
+        set.remove("JDBC");
+        set.remove("iBATIS");
+
+        System.out.println("총 객체수 : " + set.size());
+
+        iterator = set.iterator();
+        for(String element : set) {
+            System.out.println("\t" + element);
+        }
+
+        set.clear();
+        if(set.isEmpty()) {
+            System.out.println("비어 있음");
+        }
+    }
+}
---
+++ src/collectionframework/Member.java
@@ -0,0 +1,13 @@
+package collectionframework;
+
+public class Member {
+    public String name;
+    public int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
